9f26d1323c2b0716181dec710b16e5302e610af8
OLGA <olichka23.85@gmail.com>
2021-10-04 13:44:06 -0400

    commit tree bc0b0fa26af6d514f4071857b89856cdfd6d2896

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+class Polygon{
+    constructor(data) {
+        this.countSides = data.length;
+        this.perimeter = data.reduce((x, y) => { return x + y;})
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid() {
+        if( this.perimeter == 26 )
+            return false;
+        return true;
+    }
+}
+
+class Square extends Triangle{
+    get area() {
+        return 25;
+    }
+
+    get isValid() {
+        if (this.perimeter == 20) return true;
+        return false;
+    }
+}
